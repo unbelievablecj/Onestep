6a62cb303c0505ba37ee84191344cc772ed9f68c
wyz0918 <wuyizhao2008@126.com>
2018-12-19 20:54:46 +0800

    Merge branch 'master' of https://github.com/unbelievablecj/Onestep
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/java/com/example/administrator/view/CommentActivity.java

--- app/src/main/java/com/example/administrator/view/DotStrategyActivity.java
+++ app/src/main/java/com/example/administrator/view/DotStrategyActivity.java
@@ -1,7 +1,16 @@
 package com.example.administrator.view;
 
-public class DotStrategyActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class DotStrategyActivity extends AppCompatActivity {
 
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout);
 
+    }
 }
