93c40d0bf412badd4966c1850929db322d20c5f8
wyz0918 <wuyizhao2008@126.com>
2018-12-20 21:40:51 +0800

    feat:增加PicJson

---
+++ app/src/main/java/com/example/administrator/connect/PicJson.java
@@ -0,0 +1,22 @@
+package com.example.administrator.connect;
+
+public class PicJson {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    private String name;
+    private String url;
+}
--- app/src/main/java/com/example/administrator/model/Picture.java
+++ app/src/main/java/com/example/administrator/model/Picture.java
@@ -1,10 +1,17 @@
 package com.example.administrator.model;
 
+import com.example.administrator.connect.PicJson;
+
 import java.io.Serializable;
 
 public class Picture implements Serializable {
     private static final long serialVersionUID = 1L;
     private byte[] bitmapBytes = null;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     private String name = null;
     private String url;
 
@@ -14,6 +21,11 @@ public class Picture implements Serializable {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+
+    public Picture(){
+
     }
 
     public Picture(byte[] bitmapBytes, String name) {
