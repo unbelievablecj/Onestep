a53d2288fe7c7c479df5e04979ee025e05b473b3
wyz0918 <wuyizhao2008@126.com>
2018-12-21 21:12:17 +0800

    feat：增加较为完善的功能

--- app/src/main/java/com/example/administrator/view/ShareSubmitActivity.java
+++ app/src/main/java/com/example/administrator/view/ShareSubmitActivity.java
@@ -114,11 +114,11 @@ public class ShareSubmitActivity extends AppCompatActivity {
                 strategy.setTitle(title.getText().toString());
                 strategy.setLabel(labelContent);
 
-//
-//                Bitmap b = PictureUtil.compressSampling("savePic201812200612364251.JPEG");
-//                Picture picture = new Picture(PictureUtil.getBytes(b),"saveaa");
-//
-//                strategy.setPicture(picture);
+
+                Bitmap b = PictureUtil.compressSampling("savePic201812200612364251.JPEG");
+                Picture picture = new Picture(PictureUtil.getBytes(b),"saveaa");
+
+                strategy.setPicture(picture);
 
 
                 try {
